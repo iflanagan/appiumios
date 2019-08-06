9489c7fec8da14e48b4409919d18110a113a119c
iflanagan <ianianf@yahoo.com>
2019-08-06 08:00:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+## RDC-Java-TestNG-Appium-iOS-Dynamic
+
+>This code is presented as an example only, since your tests and testing environments may require specialized scripting. This information should be taken only as an
+>illustration of how one would set up tests with Sauce Labs, and any modifications will not be supported. For questions regarding Sauce Labs integration, please see 
+>our documentation at https://wiki.saucelabs.com/.
+
+### Environment Setup
+
+1. Global Dependencies
+    * Install Maven
+    	https://maven.apache.org/install.html
+    * Or Install Maven with Homebrew
+    	http://brew.sh/
+    ```
+    $ brew install maven
+    ```
+2.  TestObject Credentials and Appium Endpoint
+    * Export your API key to authenticate to TestObject
+    ```
+    $ export TESTOBJECT_API_KEY=<your_api_key>
+    ```
+    
+    * Export the TestObject Appium endpoint of your choosing
+    ```
+    $ export APPIUM_URL=<appium_endpoint_url>
+    ```
+3. TestObject Credentials
+    * Ensure that your account has access to the devices below. If not, adjust accordingly
+    ```
+    new Object[]{"iOS", "iPhone 7", "10"},
+    new Object[]{"iOS", "iPhone 8", "11"}
+    ```
+
+4. Project Dependencies
+    * Check that packages are available
+    ```
+    $ cd RDC-Java-TestNG-Appium-iOS-Dynamic
+    $ mvn test-compile
+    ```
+    * You may also want to run the command below to check for outdated dependencies. Please be sure to verify and review updates before editing your pom.xml file as they may not be compatible with your code.
+    ```
+    $ mvn versions:display-dependency-updates
+    ```
+4. Application
+    * Application is located within this repository.
+    
+### Running Tests
+```
+$ mvn test
+```
+
+### Advice/Troubleshooting
+1. It may be useful to use a Java IDE such as IntelliJ or Eclipse to help troubleshoot potential issues. 
+
+### Resources
+##### [TestObject REST API Documentation](https://api.testobject.com/)
+
+##### [Sauce Labs Documentation](https://wiki.saucelabs.com/)
+
+##### [TestNG Documentation](http://testng.org/doc/documentation-main.html)
+
+##### [Java Documentation](https://docs.oracle.com/javase/7/docs/api/)
+
+##### [Stack Overflow](http://stackoverflow.com/)
+* A great resource to search for issues not explicitly covered by documentation.
+
+
+
+
+
+
+
+# appiumios
---
+++ src/test/java/com/saucelabs/example/TestSetup.java
@@ -0,0 +1,111 @@
+package com.saucelabs.example;
+
+import com.saucelabs.example.util.ResultReporter;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.openqa.selenium.OutputType;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by grago on 28/02/2017.
+ */
+public class TestSetup {
+
+    private ResultReporter reporter;
+    private ThreadLocal<IOSDriver> driver = new ThreadLocal<IOSDriver>();
+  
+  /**
+   * DataProvider that explicitly sets the browser combinations to be used.
+   *
+   * @param testMethod
+   * @return
+   */
+  @DataProvider(name = "devices", parallel = true)
+  public static Object[][] sauceBrowserDataProvider(Method testMethod) {
+      return new Object[][]{
+    		  //Verify that your account has access to the devices below
+             new Object[]{"iOS", "iPhone 7", "10"},
+             new Object[]{"iOS", "iPhone 8", "11"}
+      };
+  }  
+  
+  private IOSDriver createDriver(String platformName, String platformVersion, String deviceName, String methodName) throws MalformedURLException {
+  	
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY"));
+      capabilities.setCapability("deviceName", deviceName);
+      capabilities.setCapability("platformVersion", platformVersion);
+      capabilities.setCapability("platformName", platformName);
+      capabilities.setCapability("name",  methodName);
+//      capabilities.setCapability("appiumVersion", "1.7.2");
+      capabilities.setCapability("appiumVersion", "1.13.0");
+      
+      driver.set(new IOSDriver<WebElement>(
+              new URL(System.getenv("APPIUM_URL")),
+              capabilities));
+      return driver.get();
+  }
+
+    /* A simple addition, it expects the correct result to appear in the result field. */
+    @Test(dataProvider = "devices")
+    public void IanFtwoPlusThreeOperation(String platformName, String deviceName, String platformVersion, Method method) throws MalformedURLException {
+
+    	IOSDriver driver = createDriver(platformName, platformVersion, deviceName, method.getName());
+
+        /* Get the elements. */
+        MobileElement buttonTwo = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("2")));
+        MobileElement buttonThree = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("3")));
+        MobileElement buttonPlus = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("+")));
+        MobileElement buttonEquals = (MobileElement)(driver.findElement(MobileBy.AccessibilityId("=")));
+        MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//XCUIElementTypeStaticText|//UIAApplication[1]/UIAWindow[1]/UIAStaticText[1]")));
+
+        /* Add two and two. */
+        buttonTwo.click();
+        buttonPlus.click();
+        buttonThree.click();
+        driver.getScreenshotAs(OutputType.FILE);
+        buttonEquals.click();
+        driver.getScreenshotAs(OutputType.FILE);
+        
+        /* Check if within given time the correct result appears in the designated field. */
+        (new WebDriverWait(driver, 30)).until(ExpectedConditions.textToBePresentInElement(resultField, "5"));
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+    	IOSDriver driver = getWebDriver();
+    	reporter = new ResultReporter();
+        boolean success = result.isSuccess();
+        String sessionId = driver.getSessionId().toString();
+
+        reporter.saveTestStatus(sessionId, success);
+        driver.quit();
+    }
+    
+    /**
+     * @return the {@link WebDriver} for the current thread
+     */
+    public IOSDriver getWebDriver() {
+        return driver.get();
+    }
+}
